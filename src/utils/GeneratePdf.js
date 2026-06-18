13cd7dda75bab2fd8c52c2e21b04f15891137035
M4RK <yoldimarcos@gmail.com>
2026-06-18 13:24:21 -0300

    print pdf

--- src/utils/GeneratePdf.js
+++ src/utils/GeneratePdf.js
@@ -2,6 +2,7 @@ import { jsPDF } from "jspdf";
 
 export const generarPDF = (turno) => {
 
+    /*
     const ahora = new Date();
     const fechaActual = ahora.toLocaleDateString();
     const horaActual = ahora.toLocaleTimeString();
@@ -26,7 +27,45 @@ export const generarPDF = (turno) => {
     ], 70, 30);
 
     /*doc.text("Hola, este es tu PDF generado desde React", 10, 10);*/
+    //doc.output("dataurlnewwindow");*/
 
-    doc.output("dataurlnewwindow");
+    //const contenido = document.getElementById("print-area").innerHTML;
+
+    const ahora = new Date();
+    const fechaActual = ahora.toLocaleDateString();
+    const horaActual = ahora.toLocaleTimeString();
+
+    const win = window.open("", "_blank");
+
+    /*
+    win.document.write(`
+    <html>
+      <body onload="window.print(); window.close();">
+        <h2>🧾 IMPRESIÓN DE PRUEBA</h2>
+        <p>Este es un test de impresión</p>
+        <p>Fecha: ${new Date().toLocaleString()}</p>
+      </body>
+    </html>
+    `);*/
+
+    win.document.write(`
+    <html>
+      <body onload="window.print(); window.close();">
+        <h2>UNIBER</h2>
+        <p>----------------------------------</p>
+        <p>TURNO N°: ${turno}</p>
+        <p>SECTOR: CAJA</p>
+        <p>TRÁMITE: PAGOS</p>
+        <p>Fecha: ${fechaActual}</p>
+        <p>Hora: ${horaActual}</p>
+        <p>----------------------------------</p>
+        <p>Por favor espere a ser llamado</p>
+        <p>en pantalla o por altavoz.</p>
+        <p>----------------------------------</p>
+        <p>Gracias por su visita</p>
+      </body>
+    </html>`);
+
+    win.document.close();
 };
 
