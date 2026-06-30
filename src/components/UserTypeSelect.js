fa6c9dca72ddae8d350bf50c1a5f29724ae0e283
M4RK <yoldimarcos@gmail.com>
2026-06-30 08:56:40 -0300

    userTypeSelect update

--- src/components/UserTypeSelect.js
+++ src/components/UserTypeSelect.js
@@ -3,13 +3,13 @@ import React from "react";
 function UserTypeSelect(props) {
   return (
       <select
-        style={{fontFamily: "Inter, sans-serif"}}
+        style={{fontFamily: "Inter, sans-serif", width:'133px'}}
         id="userType"
         className="form-select shadow-sm"
         //value={value}
         onChange={(e) => props.handleChange(props.idUser, e.target.value)}
       >
-        <option value="">Seleccione un Rol</option>
+        <option value="">Seleccione</option>
 
         {props.array.map((type) => (
           type.activo === 1 ? 
